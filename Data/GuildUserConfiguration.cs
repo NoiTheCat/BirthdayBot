488685bc79d9c1aff67fccc7c4a22df6bafd67f8
Noi <noithecat@protonmail.com>
2020-07-16 13:21:33 -0700

    Rewrite GuildUserConfiguration
    Corresponding to GuildConfiguration, no longer assumes it is cached.

--- Data/GuildUserConfiguration.cs
+++ Data/GuildUserConfiguration.cs
@@ -8,161 +8,143 @@ using System.Threading.Tasks;
 namespace BirthdayBot.Data
 {
     /// <summary>
-    /// Representation of a user's birthday settings within a guild.
-    /// Instances are held and managed by <see cref="="GuildStateInformation"/>.
+    /// Represents configuration for a guild user.
     /// </summary>
-    class GuildUserSettings
+    class GuildUserConfiguration
     {
-        private int _month;
-        private int _day;
-        private string _tz;
-
         public ulong GuildId { get; }
         public ulong UserId { get; }
 
         /// <summary>
         /// Month of birth as a numeric value. Range 1-12.
         /// </summary>
-        public int BirthMonth { get { return _month; } }
+        public int BirthMonth { get; private set; }
         /// <summary>
         /// Day of birth as a numeric value. Ranges between 1-31 or lower based on month value.
         /// </summary>
-        public int BirthDay { get { return _day; } }
+        public int BirthDay { get; private set; }
 
-        public string TimeZone { get { return _tz; } }
-        public bool IsKnown { get { return _month != 0 && _day != 0; } }
+        public string TimeZone { get; private set; }
+        public bool IsKnown { get { return BirthMonth != 0 && BirthDay != 0; } }
 
         /// <summary>
-        /// Creates a data-less instance without any useful information.
+        /// Creates a new, data-less instance without a corresponding database entry.
         /// Calling <see cref="UpdateAsync(int, int, int)"/> will create a real database enty
         /// </summary>
-        public GuildUserSettings(ulong guildId, ulong userId)
+        private GuildUserConfiguration(ulong guildId, ulong userId)
         {
             GuildId = guildId;
             UserId = userId;
         }
 
         // Called by GetGuildUsersAsync. Double-check ordinals when changes are made.
-        private GuildUserSettings(DbDataReader reader)
+        private GuildUserConfiguration(DbDataReader reader)
         {
             GuildId = (ulong)reader.GetInt64(0);
             UserId = (ulong)reader.GetInt64(1);
-            _month = reader.GetInt32(2);
-            _day = reader.GetInt32(3);
-            if (!reader.IsDBNull(4)) _tz = reader.GetString(4);
+            BirthMonth = reader.GetInt32(2);
+            BirthDay = reader.GetInt32(3);
+            if (!reader.IsDBNull(4)) TimeZone = reader.GetString(4);
         }
 
         /// <summary>
         /// Updates user with given information.
-        /// NOTE: If there exists a tz value and the update contains none, the old tz value is retained.
         /// </summary>
-        public async Task UpdateAsync(int month, int day, string newtz, Database dbconfig)
+        public async Task UpdateAsync(int month, int day, string newtz)
         {
-            // TODO note from rewrite: huh? why are we doing this here?
-            var inserttz = newtz ?? TimeZone;
-
-            using (var db = await dbconfig.OpenConnectionAsync())
+            using (var db = await Database.OpenConnectionAsync())
             {
-                // Will do a delete/insert instead of insert...on conflict update. Because lazy.
-                using (var t = db.BeginTransaction())
-                {
-                    await DoDeleteAsync(db);
-                    using (var c = db.CreateCommand())
-                    {
-                        c.CommandText = $"insert into {BackingTable} "
-                            + "(guild_id, user_id, birth_month, birth_day, time_zone) values "
-                            + "(@Gid, @Uid, @Month, @Day, @Tz)";
-                        c.Parameters.Add("@Gid", NpgsqlDbType.Bigint).Value = (long)GuildId;
-                        c.Parameters.Add("@Uid", NpgsqlDbType.Bigint).Value = (long)UserId;
-                        c.Parameters.Add("@Month", NpgsqlDbType.Numeric).Value = month;
-                        c.Parameters.Add("@Day", NpgsqlDbType.Numeric).Value = day;
-                        var p = c.Parameters.Add("@Tz", NpgsqlDbType.Text);
-                        if (inserttz != null) p.Value = inserttz;
-                        else p.Value = DBNull.Value;
-                        c.Prepare();
-                        await c.ExecuteNonQueryAsync();
-                    }
-                    await t.CommitAsync();
-                }
+                using var c = db.CreateCommand();
+                c.CommandText = $"insert into {BackingTable} "
+                    + "(guild_id, user_id, birth_month, birth_day, time_zone) values "
+                    + "(@Gid, @Uid, @Month, @Day, @Tz) "
+                    + "on conflict (guild_id, user_id) do update "
+                    + "set birth_month = EXCLUDED.birth_month, birth_day = EXCLUDED.birth_day, time_zone = EXCLUDED.time_zone";
+                c.Parameters.Add("@Gid", NpgsqlDbType.Bigint).Value = (long)GuildId;
+                c.Parameters.Add("@Uid", NpgsqlDbType.Bigint).Value = (long)UserId;
+                c.Parameters.Add("@Month", NpgsqlDbType.Numeric).Value = month;
+                c.Parameters.Add("@Day", NpgsqlDbType.Numeric).Value = day;
+                var tzp = c.Parameters.Add("@Tz", NpgsqlDbType.Text);
+                if (newtz != null) tzp.Value = newtz;
+                else tzp.Value = DBNull.Value;
+                c.Prepare();
+                await c.ExecuteNonQueryAsync();
             }
 
-            // We didn't crash! Get the new values stored locally.
-            _month = month;
-            _day = day;
-            _tz = inserttz;
+            // Database update succeeded; update instance values
+            BirthMonth = month;
+            BirthDay = day;
+            TimeZone = newtz;
         }
 
         /// <summary>
         /// Deletes information of this user from the backing database.
         /// The corresponding object reference should ideally be discarded after calling this.
         /// </summary>
-        public async Task DeleteAsync(Database dbconfig)
+        public async Task DeleteAsync()
         {
-            using (var db = await dbconfig.OpenConnectionAsync())
-            {
-                await DoDeleteAsync(db);
-            }
-        }
-
-        // Shared between UpdateAsync and DeleteAsync
-        private async Task DoDeleteAsync(NpgsqlConnection dbconn)
-        {
-            using (var c = dbconn.CreateCommand())
-            {
-                c.CommandText = $"delete from {BackingTable} "
-                    + "where guild_id = @Gid and user_id = @Uid";
-                c.Parameters.Add("@Gid", NpgsqlDbType.Bigint).Value = (long)GuildId;
-                c.Parameters.Add("@Uid", NpgsqlDbType.Bigint).Value = (long)UserId;
-                c.Prepare();
-                await c.ExecuteNonQueryAsync();
-            }
+            using var db = await Database.OpenConnectionAsync();
+            using var c = db.CreateCommand();
+            c.CommandText = $"delete from {BackingTable} "
+                + "where guild_id = @Gid and user_id = @Uid";
+            c.Parameters.Add("@Gid", NpgsqlDbType.Bigint).Value = (long)GuildId;
+            c.Parameters.Add("@Uid", NpgsqlDbType.Bigint).Value = (long)UserId;
+            c.Prepare();
+            await c.ExecuteNonQueryAsync();
         }
 
         #region Database
         public const string BackingTable = "user_birthdays";
+        // Take note of ordinals for use in the constructor
+        private const string SelectFields = "guild_id, user_id, birth_month, birth_day, time_zone";
 
-        internal static void SetUpDatabaseTable(NpgsqlConnection db)
+        internal static async Task DatabaseSetupAsync(NpgsqlConnection db)
         {
-            using (var c = db.CreateCommand())
-            {
-                c.CommandText = $"create table if not exists {BackingTable} ("
-                    + $"guild_id bigint not null references {GuildStateInformation.BackingTable} ON DELETE CASCADE, "
-                    + "user_id bigint not null, "
-                    + "birth_month integer not null, "
-                    + "birth_day integer not null, "
-                    + "time_zone text null, "
-                    + "last_seen timestamptz not null default NOW(), "
-                    + "PRIMARY KEY (guild_id, user_id)"
-                    + ")";
-                c.ExecuteNonQuery();
-            }
+            using var c = db.CreateCommand();
+            c.CommandText = $"create table if not exists {BackingTable} ("
+                + $"guild_id bigint not null references {GuildConfiguration.BackingTable} ON DELETE CASCADE, "
+                + "user_id bigint not null, "
+                + "birth_month integer not null, "
+                + "birth_day integer not null, "
+                + "time_zone text null, "
+                + "last_seen timestamptz not null default NOW(), "
+                + "PRIMARY KEY (guild_id, user_id)" // index automatically created with this
+                + ")";
+            await c.ExecuteNonQueryAsync();
         }
 
         /// <summary>
-        /// Gets all known birthday records from the specified guild. No further filtering is done here.
+        /// Attempts to retrieve a user's configuration. Returns a new, updateable instance if none is found.
         /// </summary>
-        internal static IEnumerable<GuildUserSettings> GetGuildUsersAsync(Database dbsettings, ulong guildId)
+        public static async Task<GuildUserConfiguration> LoadAsync(ulong guildId, ulong userId)
         {
-            using (var db = dbsettings.OpenConnectionAsync().GetAwaiter().GetResult())
-            {
-                using (var c = db.CreateCommand())
-                {
-                    // Take note of ordinals for use in the constructor
-                    c.CommandText = "select guild_id, user_id, birth_month, birth_day, time_zone "
-                        + $"from {BackingTable} where guild_id = @Gid";
-                    c.Parameters.Add("@Gid", NpgsqlDbType.Bigint).Value = (long)guildId;
-                    c.Prepare();
-                    using (var r = c.ExecuteReader())
-                    {
-                        var result = new List<GuildUserSettings>();
-                        while (r.Read())
-                        {
-                            result.Add(new GuildUserSettings(r));
-                        }
-                        return result;
-                    }
-                }
-            }
+            using var db = await Database.OpenConnectionAsync();
+            using var c = db.CreateCommand();
+            c.CommandText = $"select {SelectFields} from {BackingTable} where guild_id = @Gid and user_id = @Uid";
+            c.Parameters.Add("@Gid", NpgsqlDbType.Bigint).Value = (long)guildId;
+            c.Parameters.Add("@Uid", NpgsqlDbType.Bigint).Value = (long)userId;
+            c.Prepare();
+
+            using var r = c.ExecuteReader();
+            if (await r.ReadAsync()) return new GuildUserConfiguration(r);
+            else return new GuildUserConfiguration(guildId, userId);
+        }
+
+        /// <summary>
+        /// Gets all known user configuration records associated with the specified guild.
+        /// </summary>
+        public static async Task<IEnumerable<GuildUserConfiguration>> LoadAllAsync(ulong guildId)
+        {
+            using var db = await Database.OpenConnectionAsync();
+            using var c = db.CreateCommand();
+            c.CommandText = $"select {SelectFields} from {BackingTable} where guild_id = @Gid";
+            c.Parameters.Add("@Gid", NpgsqlDbType.Bigint).Value = (long)guildId;
+            c.Prepare();
+
+            using var r = await c.ExecuteReaderAsync();
+            var result = new List<GuildUserConfiguration>();
+            while (await r.ReadAsync()) result.Add(new GuildUserConfiguration(r));
+            return result;
         }
         #endregion
     }
