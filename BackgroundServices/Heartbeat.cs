f1e7bd8c6738d315afb1c1e34bd450e69cfc54ab
Noi <noithecat@protonmail.com>
2020-07-27 22:40:09 -0700

    Removed disconnected server count
    Made redundant by per-shard status on every birthday update

--- BackgroundServices/Heartbeat.cs
+++ BackgroundServices/Heartbeat.cs
@@ -14,18 +14,6 @@ namespace BirthdayBot.BackgroundServices
         {
             var uptime = DateTimeOffset.UtcNow - Program.BotStartTime;
             Log($"Bot uptime: {Common.BotUptime}");
-
-            // Disconnection warn
-            foreach (var shard in BotInstance.DiscordClient.Shards)
-            {
-                if (shard.ConnectionState == Discord.ConnectionState.Disconnected)
-                {
-                    Log($"Shard {shard.ShardId} is disconnected! Restart the app if this persists.");
-                    // The library alone cannot be restarted as it is in an unknown state. It was not designed to be restarted.
-                    // TODO This is the part where we'd signal something to restart us if we were fancy.
-                }
-            }
-
             return Task.CompletedTask;
         }
     }
