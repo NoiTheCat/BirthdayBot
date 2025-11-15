72611e408cb9e32499f2170cbbc209d951ae9ca7
Noi <noi@noithecat.dev>
2025-11-14 19:33:48 -0800

    Caching and lazy updating of base values

--- ApplicationCommands/TzAutocompleteHandler.cs
+++ ApplicationCommands/TzAutocompleteHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using BirthdayBot.Data;
 using Discord.Interactions;
 using Microsoft.EntityFrameworkCore;
@@ -5,9 +6,17 @@ using NodaTime;
 
 namespace BirthdayBot.ApplicationCommands;
 public class TzAutocompleteHandler : AutocompleteHandler {
-    private static readonly IReadOnlyCollection<string> AllTimeZones;
+    private static readonly TimeSpan _maxListAge = TimeSpan.FromHours(6);
+    private static readonly ReaderWriterLockSlim _lock = new();
+    private static ReadOnlyCollection<string> _baseZonesList;
+    private static DateTimeOffset _lastListUpdate;
 
     static TzAutocompleteHandler() {
+        _baseZonesList = RebuildSuggestionBaseList();
+        _lastListUpdate = DateTimeOffset.UtcNow;
+    }
+
+    private static ReadOnlyCollection<string> RebuildSuggestionBaseList() {
         // This bot discourages use of certain zone names and prefer the typical Region/City format over individual countries.
         // They have been excluded from this autocomplete list.
         var canonicalZones = DateTimeZoneProviders.Tzdb.Ids
@@ -23,13 +32,8 @@ public class TzAutocompleteHandler : AutocompleteHandler {
                 || z.StartsWith("Etc/")
                 || z == "UTC"
                 || z == "GMT")
-            .Distinct();
-        AllTimeZones = new HashSet<string>(canonicalZones!);
-    }
-    public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext cx,
-                                                                        IAutocompleteInteraction ia, IParameterInfo pm,
-                                                                        IServiceProvider sv) {
-        // Get our table sorted out
+            .Distinct()
+            .ToHashSet();
 
         // List of zones by current popularity
         var db = new BotDatabaseContext();
@@ -39,7 +43,7 @@ public class TzAutocompleteHandler : AutocompleteHandler {
             .ToList();
 
         // Left join: left = all NodaTime canonical zones, right = zones plus popularity data
-        var withAllZones = AllTimeZones.GroupJoin(tzPopCount, 
+        var withAllZones = canonicalZones.GroupJoin(tzPopCount,
             left => left,
             right => right.ZoneName,
             (tz, group) => new {
@@ -48,32 +52,63 @@ public class TzAutocompleteHandler : AutocompleteHandler {
             })
             .ToList();
 
-        // Remove all non-canonical zones, do sorting
-        var resultList = withAllZones
-            .Where(z => AllTimeZones.Contains(z.ZoneName))
-            .OrderByDescending(z => z.Count)
-            .ToList();
-        // TODO cache and refresh this periodically instead of recomputing every time
+        // Remove all non-canonical zones, sort by popularity
+        return withAllZones
+                .Where(z => canonicalZones.Contains(z.ZoneName))
+                .OrderByDescending(z => z.Count)
+                .Select(z => z.ZoneName)
+                .ToList()
+                .AsReadOnly();
+    }
+
+    private static ReadOnlyCollection<string> GetBaseList() {
+        _lock.EnterUpgradeableReadLock();
+        try {
+            // Should regenerate base list?
+            var now = DateTimeOffset.UtcNow;
+            if (now - _lastListUpdate > _maxListAge) {
+                _lock.EnterWriteLock();
+                try {
+                    // Double-check in the write thread - in case another took the write lock just before us
+                    if (now - _lastListUpdate > _maxListAge) {
+                        _baseZonesList = RebuildSuggestionBaseList();
+                        _lastListUpdate = now;
+                    }
+                } finally {
+                    _lock.ExitWriteLock();
+                }
+            }
+            return _baseZonesList;
+        } finally {
+            _lock.ExitUpgradeableReadLock();
+        }
+    }
+
+    public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext cx,
+                                                                        IAutocompleteInteraction ia, IParameterInfo pm,
+                                                                        IServiceProvider sv) {
+        var resultList = GetBaseList();
 
         // Filter from existing input, give results
         var input = ((SocketAutocompleteInteraction)ia).Data.Current.Value.ToString()!;
         var inputsplit = input.Split('/', 2, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
         var result = resultList
             .Where(r => {
-                var tzsplit = r.ZoneName.Split('/', 2, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                var tzsplit = r.Split('/', 2, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
                 if (inputsplit.Length == 2) {
                     if (tzsplit.Length == 1) return false;
                     return tzsplit[0].Contains(inputsplit[0], StringComparison.OrdinalIgnoreCase)
                         && tzsplit[1].Contains(inputsplit[1], StringComparison.OrdinalIgnoreCase);
                 } else {
-                    // No '/' in query - search for string within each side of zone name (tested to not give conflicting results)
+                    // No '/' in query - search for string within each side of zone name
+                    // Testing confirms this does not give conflicting results
                     if (tzsplit.Length == 1) return tzsplit[0].Contains(input, StringComparison.OrdinalIgnoreCase);
                     else return tzsplit[0].Contains(input, StringComparison.OrdinalIgnoreCase)
                         || tzsplit[1].Contains(input, StringComparison.OrdinalIgnoreCase);
                 }
             })
             .Take(25)
-            .Select(x => new AutocompleteResult(x.ZoneName, x.ZoneName))
+            .Select(z => new AutocompleteResult(z, z))
             .ToList();
         return Task.FromResult(AutocompletionResult.FromSuccess(result));
     }
